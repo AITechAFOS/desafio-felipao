39c394c536070fd4b0cc48eaa751fdaf31e00461
AITechAFOS <aitech.afos@gmail.com>
2024-11-13 16:35:03 -0300

    commit

--- criando_primeira_funcao/06_funcoes_com_retorno.js
+++ criando_primeira_funcao/06_funcoes_com_retorno.js
@@ -1,11 +1,9 @@
-let resultado = soma(5,5)           //Atribuindo para uma variável, o resultado de uma função com 2 parâmetros
+let resultado = soma(5,5)                    //Atribuindo para uma variável, o resultado de uma função com 2 parâmetros
 console.log("O resultado da função é: " + resultado)
-function soma(numA,numB){           //Declaração de uma Função com 2 parâmetros
-     let somatorio = numA + numB    //Criando uma variável e atribuindo a soma dos 2 parâmetros
-     return somatorio               //Retorna o produto da função para quem o chamou, neste caso o
-}                                   //let resultado
+function soma(numA,numB){                    //Declaração de uma Função com 2 parâmetros
+     let primeiroSomatorio = [3, 11, 90]     //Atribindo uma variárel para receber um vetor
+     let segundoSomatorio = [2,100,30]
+     return primeiroSomatorio, segundoSomatorio }              /*Retornando o produto da função diretamente para quem o chamou,
+                                             desconsiderando o produto dos 2 parâmetros recebidos*/
+                                    
 
-/*
-let VARIÁVEL = CHAMADA_FUNCAO(PARAMETRO1,PARAMETRO2)
-FUNCTION NOME_DA_FUNCAO(VARIAVEL1,VARIAVEL2){
-} */
